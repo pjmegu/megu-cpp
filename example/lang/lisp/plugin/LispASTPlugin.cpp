4c3fa6a3ad23fbde351d26269e5991e56988b396
sakura_aoi <souyakoji@gmail.com>
2024-08-27 09:01:25 +0000

    add plugin

--- example/lang/lisp/plugin/LispASTPlugin.cpp
+++ example/lang/lisp/plugin/LispASTPlugin.cpp
@@ -1,7 +1,7 @@
 #include "mlir/IR/DialectRegistry.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/Tools/Plugins/DialectPlugin.h"
-#include "mokey/MokeyToken/MokeyTokenDialect.h"
+#include "lisp/LispAST/LispASTDialect.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Config/llvm-config.h"
 
@@ -11,10 +11,10 @@ extern "C" LLVM_ATTRIBUTE_WEAK DialectPluginLibraryInfo
 mlirGetDialectPluginInfo() {
     return {
         MLIR_PLUGIN_API_VERSION,
-        "mokey.token",
+        "mokey.example.lisp.ast",
         LLVM_VERSION_STRING,
         [](DialectRegistry *registry) {
-            registry->insert<mokey::token::MokeyTokenDialect>();
+            registry->insert<mokey::example::lisp::ast::LispASTDialect>();
         }
     };
 }
